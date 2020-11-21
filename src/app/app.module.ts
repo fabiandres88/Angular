235ac4ea94ace874f802b4c295358edb635fce4a
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-11-20 19:05:19 -0500

    Using multiples components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FruitComponent } from './fruit/fruit.component';
-
+import { EmployeeComponent } from './employee/employee.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FruitComponent
+    FruitComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
