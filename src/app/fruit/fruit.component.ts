5c0e6e3d7bf7e4f079b1c51fae039b5b5d73fa5b
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-11-20 21:18:16 -0500

    Adding Constructor and ngOnInit to the component class

--- src/app/fruit/fruit.component.ts
+++ src/app/fruit/fruit.component.ts
@@ -4,12 +4,33 @@ import { Component } from '@angular/core';
     selector: 'fruit',
     templateUrl: './fruit.component.html'
 })
-export class FruitComponent{
+export class FruitComponent {
     public component_name = 'Fruit component';
     public fruits_list = 'Orange, Apple, Pear and Watermelon';
-    public name:string = 'Fabian Ramirez';
-    public age:number = 31;
-    public Old:boolean = true;
-    public interest:Array<any> = ['Development','Football',5];
-    anyProperty:any = 'Web Developer';
+    public name: string;
+    public age: number;
+    public Old: boolean;
+    public interest: Array<any> = ['Development', 'Football', 5];
+    anyProperty: any;
+
+    constructor() {
+        this.name = 'Fabian Ramirez';
+        this.age = 31;
+        this.Old = false;
+        this.anyProperty = 'Web developer';
+    }
+
+    ngOnInit() {
+        this.helloWorld(this.name);
+        this.changeName();
+        console.log(this.name);
+    }
+
+    helloWorld(name: string) {
+        alert(`Fisrt Angular Method used by: ${name}`);
+    }
+
+    changeName() {
+        this.name = 'Carolina Jaimes';
+    }
 }
